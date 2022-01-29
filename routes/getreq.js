fc2224dae1c600770e007623efbbd7ca3f33ac1d
prgayake <prgayake100@gmail.com>
2022-01-29 20:02:36 +0530

    auth fixed

--- routes/getreq.js
+++ routes/getreq.js
@@ -12,10 +12,28 @@ const firebaseConfig = {
     messagingSenderId: "772756370856",
     appId: "1:772756370856:web:b69242c1dc4e9f66ba599d"
 };
-
+firebase.initializeApp(firebaseConfig);
 
 router.post('/login', (req, res) => {
-   console.log(req.body)
+    
+    firebase.auth().signInWithEmailAndPassword(req.body.email, req.body.password).then(() => {
+        res.redirect('/');
+    }).catch((error) => {
+        res.send(error.message);
+    });
 });
+
+router.post('/signup', (req, res) => {
+    
+    firebase.auth().createUserWithEmailAndPassword(req.body.email, req.body.password).then(() => {
+        res.redirect('/login');
+    }).catch((error) => {
+        res.send(error.message);
+    });
+});
+
+router.get('/reset',(req,res)=>{
+    
+})
 
 module.exports = router;
